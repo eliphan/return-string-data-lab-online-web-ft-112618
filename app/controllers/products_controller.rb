ce074334f68d300547e024c17942e269925cf465
Elise Phan <aliceconnic@gmail.com>
2019-03-28 20:20:56 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   
-  def new 
-    @product = Product.new 
-  end 
-  
+  def new
+    @product = Product.new
+  end
+
   def create
-    @product = Product.create(product_params)
+    Product.create(product_params)
     redirect_to products_path
   end
   
